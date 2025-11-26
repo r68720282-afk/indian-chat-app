90dad3ae80c3e2a94c755d3d3c9134f4a599ab66
r68720282-afk <r68720282@gmail.com>
2025-11-26 16:59:16 +0530

    Update rooms.routes.js

--- chatsite_complete/server/routes/rooms.routes.js
+++ chatsite_complete/server/routes/rooms.routes.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const rooms = ["general", "tech", "random"];
+
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+    rooms
+  });
+});
+
+module.exports = router;
 
