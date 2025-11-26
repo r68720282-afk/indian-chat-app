9988eb0da80a3ecdaa493441798304433ce5ac52
r68720282-afk <r68720282@gmail.com>
2025-11-27 00:04:24 +0530

    Update index.js

--- chatsite_complete/server/index.js
+++ chatsite_complete/server/index.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 const express = require('express');
 const http = require('http');
@@ -9,7 +8,7 @@ const { Server } = require('socket.io');
 // Models
 const Message = require("./models/message.model");
 
-// Connect MongoDB
+// MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("Mongo Error:", err));
@@ -18,52 +17,54 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// API Routes Import
+// API Routes
 const statusRoutes = require("./routes/status.routes");
 const roomsRoutes = require("./routes/rooms.routes");
 
-// Attach Routes
 app.use("/api/status", statusRoutes);
 app.use("/api/rooms", roomsRoutes);
 
-// Serve static public folder temporarily
+// Public folder
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/api', (req, res) => {
   res.json({ status: "Chat server running", time: Date.now() });
 });
 
-// Create server + socket
+// Server + Socket
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-// In-memory Rooms (used only for user list)
+// Rooms storage (MAP for online users)
 const rooms = {
-  general: { id: "general", users: new Set() },
-  tech: { id: "tech", users: new Set() },
-  random: { id: "random", users: new Set() }
+  general: { id: "general", users: new Map() },
+  tech: { id: "tech", users: new Map() },
+  fun: { id: "fun", users: new Map() },
+  love: { id: "love", users: new Map() },
+  gaming: { id: "gaming", users: new Map() }
 };
 
-// socket handling
+// SOCKET HANDLING
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   socket.on("joinRoom", async ({ roomId, username }) => {
     if (!roomId) roomId = "general";
 
+    // Create room if not exists
     if (!rooms[roomId]) {
-      rooms[roomId] = { id: roomId, users: new Set() };
+      rooms[roomId] = { id: roomId, users: new Map() };
     }
 
     socket.join(roomId);
     socket.data.username = username || "Guest";
     socket.data.roomId = roomId;
 
-    rooms[roomId].users.add({
-    id: socket.id,
-    name: socket.data.username
-});
-
+    // Save user in MAP
+    rooms[roomId].users.set(socket.id, {
+      id: socket.id,
+      name: socket.data.username
+    });
 
     // Load last 100 messages from MongoDB
     const recent = await Message.find({ roomId })
@@ -73,9 +74,14 @@ io.on("connection", (socket) => {
 
     socket.emit("recentMessages", recent);
 
-    // Notify room
+    // System join message
     io.to(roomId).emit("systemMessage", { text: `${socket.data.username} joined` });
-    io.to(roomId).emit("roomUsers", { users: rooms[roomId].users.size });
+
+    // Send full user list
+    io.to(roomId).emit("roomUsers", {
+      count: rooms[roomId].users.size,
+      list: Array.from(rooms[roomId].users.values())
+    });
   });
 
   socket.on("sendMessage", async ({ roomId, text }) => {
@@ -99,16 +105,23 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     const roomId = socket.data.roomId;
+
     if (roomId && rooms[roomId]) {
       rooms[roomId].users.delete(socket.id);
+
       io.to(roomId).emit("systemMessage", { text: `${socket.data.username} left` });
-      io.to(roomId).emit("roomUsers", { users: rooms[roomId].users.size });
+
+      io.to(roomId).emit("roomUsers", {
+        count: rooms[roomId].users.size,
+        list: Array.from(rooms[roomId].users.values())
+      });
     }
+
     console.log("User disconnected:", socket.id);
   });
 });
 
-// Start server
+// Start
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
